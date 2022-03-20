9e548f77007cb28b599cc59e321be1de005e6b75
Sabuhi0 <sabuhiq0@gmail.com>
2022-03-20 15:57:41 +0400

    Add categories component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
-
+import Categories from "./components/Categories";
 function App() {
   return (
     <>
       <Header/>
       <HeroSection/>
+      <Categories/>
     </>
   )
 }
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,7 +1,27 @@
-export default function Catagories() {
-    return(
-        <div>
-            Catagories
-        </div>
-    )
+import {useEffect, useState} from 'react'
+import categoryData from '../api/categories.json'
+import Category from './ui/Category';
+
+function Categories() {
+
+	const [categories, setCategories] = useState([])
+
+	useEffect(() => {
+        setTimeout(() => setCategories(categoryData), 1000)
+		// setCategories(categoryData)
+	}, [])
+
+	return (
+		<div className="bg-white py-6">
+			<div className="container mx-auto">
+				<h3 className="font-semibold text-sm mb-2">Kategoriler</h3>
+				<div className="grid grid-cols-10">
+                    {!categories.length && 'Yükleniyor..'}
+					{categories && categories.map(category => <Category key={category.id} category={category} />)}
+				</div>
+			</div>
+		</div>
+	)
 }
+
+export default Categories
