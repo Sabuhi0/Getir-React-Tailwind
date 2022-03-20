f413aac8b91df49046aa13faa4f726b0ed32f3ce
Sabuhi0 <sabuhiq0@gmail.com>
2022-03-20 15:56:56 +0400

    Add container className

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -39,7 +39,7 @@ export default function HeroSection() {
                     <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-1-757eca6a46304def60cabce74d3f20a2.jpg" alt="" />
                 </div>
             </Slider>
-            <div className="w-[1221px] flex justify-between items-center absolute top-0 left-1/2 -translate-x-1/2 h-full z-20">
+            <div className="container flex justify-between items-center absolute top-0 left-1/2 -translate-x-1/2 h-full z-20">
                 <div>
                     <img src="https://getir.com/_next/static/images/bimutluluk-b3a7fcb14fc9a9c09b60d7dc9b1b8fd6.svg" alt="" />
                     <h3 className="mt-8 text-4xl font-semibold text-white">
