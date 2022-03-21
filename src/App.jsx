75915d3c257eea9bbf5dca6e1f82a09c34c678fb
Sabuhi0 <sabuhiq0@gmail.com>
2022-03-21 17:28:03 +0400

    Add campaigns component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import Categories from "./components/Categories";
+import Campaigns from "./components/Campaigns";
 function App() {
   return (
     <>
       <Header/>
       <HeroSection/>
       <Categories/>
+      <Campaigns/>
     </>
   )
 }
