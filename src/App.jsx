e7f972e728ac6e10d450b6f455c0f74e5e105c06
Sabuhi0 <sabuhiq0@gmail.com>
2022-03-22 15:57:54 +0400

    Initial favorites component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import Categories from "./components/Categories";
 import Campaigns from "./components/Campaigns";
+import Favorites from "./components/Favorites";
 function App() {
   return (
     <>
@@ -9,6 +10,7 @@ function App() {
       <HeroSection/>
       <Categories/>
       <Campaigns/>
+      <Favorites/>
     </>
   )
 }
