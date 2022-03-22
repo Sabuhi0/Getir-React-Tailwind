d1ae4a193f792bfcf7d787574fa00438ba56a650
Sabuhi0 <sabuhiq0@gmail.com>
2022-03-22 22:32:18 +0400

    Add favorites component

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,9 +1,24 @@
+import { useState,useEffect } from 'react';
+import Products from '../api/products.json';
+import ProductItem from './ui/ProductItem';
+
 function Favorites() {
-  return (
-    <div>
-        favorites
-    </div>
-  )
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        setProducts(Products);
+    },[])
+
+    return (
+        <div className='container mx-auto'>
+            <h3 className="font-semibold text-sm mb-2">Favoriler</h3>
+            <div className="grid grid-cols-8 gap-0.1 rounded-lg overflow-hidden">
+                {products.length && products.map((product, index) => <ProductItem key={index} product={product} />)}
+
+            </div>
+        </div>
+    )
 }
 
 export default Favorites
--- src/components/ui/ProductItem.jsx
+++ src/components/ui/ProductItem.jsx
@@ -1,7 +1,15 @@
-function ProductItem() {
+import { AiOutlinePlus } from 'react-icons/ai';
+
+function ProductItem({product}) {
   return (
-    <div>
-      
+    <div className="bg-white flex flex-col gap-y-1 items-center relative text-center text-sm font-semibold p-3">
+        <button className='absolute top-3 right-3 bg-white w-8 h-8 flex items-center justify-center border border-gray-200 rounded-lg text-brand-color shadow-md'>
+            <AiOutlinePlus />
+        </button>
+        <img src={product.image} alt={product.title} />
+        <div className="text-brand-color">{product.title}</div>
+        <div className="text-gray-900">{product.price}</div>
+        <div className="text-gray-500">{product.alt}</div>
     </div>
   )
 }
