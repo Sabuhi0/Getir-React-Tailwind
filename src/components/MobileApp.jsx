bbe86afbf79e21d6bb3b077f9454740d06f9cf93
Sabuhi0 <sabuhiq0@gmail.com>
2022-03-27 20:28:13 +0400

    Update

--- src/components/MobileApp.jsx
+++ src/components/MobileApp.jsx
@@ -1,9 +1,9 @@
 function MobileApp() {
   return (
-    <div className="container mx-auto flex justify-between items-center bg-primary-brand-color bg-mobile-app mt-6 rounded-lg text-white">
+    <div className="container mx-auto flex items-center justify-between bg-primary-brand-color bg-mobile-app mt-6 rounded-lg text-white">
       <div className="flex flex-col gap-y-3 p-10">
         <h3 className="text-[26px] font-bold tracking-tight">Getir'i indirin!</h3>
-        <p className="font-semibold">İstediğiniz ürünleri dakikalar içinde kapınıza <br /> getirelim.</p>
+        <p className="font-semibold">İstediğiniz ürünleri dakikalar içinde kapınıza <br/> getirelim.</p>
         <nav className="mt-5 flex gap-x-2">
           <a href="/">
             <img src="https://getir.com/_next/static/images/appstore-tr-141ed939fceebdcee96af608fa293b31.svg" alt="" />
