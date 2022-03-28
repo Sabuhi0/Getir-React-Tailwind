b3baad41b4ae4c20d5c82a34fd6b85281fe9ce60
Sabuhi0 <sabuhiq0@gmail.com>
2022-03-28 23:15:47 +0400

    Update padding

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -14,7 +14,7 @@ function Cards() {
         <div className="container mx-auto mt-14 grid grid-cols-3 gap-x-4">
             {
                 cards.length && cards.map(card => (
-                    <div className="bg-white p-10 rounded-lg shadow-sm flex flex-col items-center text-center">
+                    <div className="bg-white p-14 rounded-lg shadow-sm flex flex-col items-center text-center">
                         <img className="w-[150px] h-[150px] mb-6" src={card.image} alt="Card" />
                         <h1 className="font-semibold text-lg text-primary-brand-color">{card.title}</h1>
                         <p className="mt-2 text-sm text-gray-700">{card.desc}</p>
