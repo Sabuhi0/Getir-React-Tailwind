79ced23bb7329f808fd82da12b517cc5bb9e71ff
Sabuhi0 <sabuhiq0@gmail.com>
2022-03-28 23:05:23 +0400

    Add Cards component

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -15,7 +15,6 @@ function Favorites() {
             <h3 className="font-semibold text-sm mb-2">Favoriler</h3>
             <div className="grid grid-cols-8 gap-0.1 rounded-lg overflow-hidden">
                 {products.length && products.map((product, index) => <ProductItem key={index} product={product} />)}
-
             </div>
         </div>
     )
