58c9c08b353d1c92396f090fbf79626a711c581b
Sabuhi0 <sabuhiq0@gmail.com>
2022-04-09 00:46:35 +0400

    Add favorites component responsive

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -13,7 +13,7 @@ function Favorites() {
     return (
         <div className='container mx-auto'>
             <h3 className="font-semibold text-sm mb-2">Favoriler</h3>
-            <div className="grid grid-cols-8 gap-0.1 rounded-lg overflow-hidden">
+            <div className="grid grid-cols-3 md:grid-cols-6 xl:grid-cols-8 2xl:grid-cols-9 gap-0.1 rounded-lg overflow-hidden">
                 {products.length && products.map((product, index) => <ProductItem key={index} product={product} />)}
             </div>
         </div>
