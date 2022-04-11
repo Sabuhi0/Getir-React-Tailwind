7a1b94c95650b7f87d34a18c16b5a22445d49089
Sabuhi0 <sabuhiq0@gmail.com>
2022-04-11 15:37:17 +0400

    Add image

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -36,10 +36,16 @@ function HeroSection() {
             <div className="relative h-auto md:h-[500px] before:bg-gradient-to-r before:from-brand-color before:to-transparent before:absolute before:inset-0 before:w-full before:h-full before:z-10">
                 { windowWidth >= 768 && <Slider {...settings}>
                         <div>
-                            <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-4-1751ad2d8fb42a88742d6751938da7e7.jpg" alt="" />
+                            <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-4-1751ad2d8fb42a88742d6751938da7e7.jpg" alt="person" />
                         </div>
                         <div>
-                            <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-1-757eca6a46304def60cabce74d3f20a2.jpg" alt="" />
+                            <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-1-757eca6a46304def60cabce74d3f20a2.jpg" alt="person" />
+                        </div>
+                        <div>
+                            <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-2-7c23764275cdaf14d7b6cf15ebbdd0c1.jpg" alt="person" />
+                        </div>
+                        <div>
+                            <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-3-ffd1e98c5da51809f9adcc3a4a37d751.jpg" alt="person" />
                         </div>
                     </Slider>
                 }
