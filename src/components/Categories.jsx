180924682d2a5c4f4569a8ac8109191a8173c3ac
Sabuhi0 <sabuhiq0@gmail.com>
2022-04-11 14:24:17 +0400

    Update categories components responsive

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -12,9 +12,9 @@ function Categories() {
 	}, [])
 
 	return (
-		<div className="bg-white py-6">
-			<div className="container mx-auto">
-				<h3 className="font-semibold text-sm mb-2">Kategoriler</h3>
+		<div className="bg-white py-6 shadow-getir">
+			<div className="md:container mx-auto">
+				<h3 className="font-semibold text-sm mb-2 px-6 md:px-0">Kategoriler</h3>
 				<div className="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10 2xl:grid-cols-12">
                     {!categories.length && 'Yükleniyor..'}
 					{categories && categories.map(category => <Category key={category.id} category={category} />)}
