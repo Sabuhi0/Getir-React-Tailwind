339172e3e2b3e1030b7cdb1118443bad12da9ab8
Sabuhi0 <sabuhiq0@gmail.com>
2022-04-12 01:14:11 +0400

    Update mobile app responsive

--- src/components/MobileApp.jsx
+++ src/components/MobileApp.jsx
@@ -1,10 +1,10 @@
 function MobileApp() {
   return (
     <div className="md:container mx-auto flex justify-between items-center bg-primary-brand-color bg-mobile-app mt-6 md:rounded-lg text-white">
-      <div className="flex flex-col text-center md:text-left gap-y-3 p-10">
+      <div className="flex flex-col text-left gap-y-3 p-6 md:p-10">
         <h3 className="text-[26px] font-bold tracking-tight">Getir'i indirin!</h3>
-        <p className="font-semibold">İstediğiniz ürünleri dakikalar içinde kapınıza <br/> getirelim.</p>
-        <nav className="mt-5 flex flex-wrap lg:flex-nowrap justify-center gap-2">
+        <p className="font-semibold md:w-[360px]">İstediğiniz ürünleri dakikalar içinde kapınıza getirelim.</p>
+        <nav className="mt-5 flex flex-wrap lg:flex-nowrap lg:justify-center gap-2">
           <a href="/">
             <img src="https://getir.com/_next/static/images/appstore-tr-141ed939fceebdcee96af608fa293b31.svg" className="md:h-8 lg:h-auto" alt="" />
           </a>
