cb2c679b2eb7747a80e83acacb182e97bcb1c41f
Sabuhi0 <sabuhiq0@gmail.com>
2022-04-14 19:12:02 +0400

    Convert normal function to arrow function

--- src/components/ui/Category.jsx
+++ src/components/ui/Category.jsx
@@ -1,4 +1,4 @@
-function Category({category: {id,title,image}}) {
+const Category = ({category: {id,title,image}}) => {
     return (
       <a href="/" className="flex flex-col items-center group gap-y-2 text-center p-4 transition hover:bg-purple-50">
           <img className="w-12 h-12 rounded border border-gray-200" src={image} alt={title}/>
