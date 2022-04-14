eab05a2a0ec03730a369da3f12fbb809e55742c0
Sabuhi0 <sabuhiq0@gmail.com>
2022-04-14 19:09:39 +0400

    Convert normal function to arrow function

--- src/components/ui/ProductItem.jsx
+++ src/components/ui/ProductItem.jsx
@@ -1,6 +1,6 @@
 import { AiOutlinePlus } from 'react-icons/ai';
 
-function ProductItem({product}) {
+const ProductItem = ({product}) => {
   return (
     <div className="bg-white flex flex-col gap-y-1 items-center relative text-center text-sm font-semibold p-3">
         <button className='absolute top-3 right-3 bg-white w-8 h-8 flex items-center justify-center border border-gray-200 rounded-lg text-brand-color shadow-md'>
